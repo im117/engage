72b7f49c3bde1ff045ac92d24c35c9bfec55caaf
joshrandall8478 <joshrandall8478@gmail.com>
2025-01-26 17:23:11 -0500

    took out unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import VideoPlayer from "./components/VideoPlayer.tsx"
 import App from "./App.tsx"
 
 createRoot(document.getElementById('root')!).render(
