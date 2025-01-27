735e7aca06aa90886a1ab4968440cb2e6fd974dc
Long Le <tlenguye@emich.edu>
2025-01-26 19:01:06 -0500

    upload style

--- src/upload.tsx
+++ src/upload.tsx
@@ -1,28 +1,27 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './upload.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import FileUploader from './components/FileUploader';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./upload.scss"; // Import the updated styles
+import FileUploader from "./components/FileUploader";
 
 function Upload() {
-
   return (
-    <>
-    <div>
-      <button onClick={() => window.location.href = '/'}>Home</button>
-      <h1>Upload your video</h1>
+    <div className="upload-container">
+      <button
+        className="back-button"
+        onClick={() => (window.location.href = "/")}
+      >
+        Home
+      </button>
+      <h1 className="upload-title">Upload Your Video</h1>
       <FileUploader />
     </div>
-    </>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <Upload />
-    </StrictMode>,
-  )
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Upload />
+  </StrictMode>
+);
