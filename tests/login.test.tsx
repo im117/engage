d33cee62ae473ebae7a895f2f31084a2e0da691a
Joshua Randall <jranda10@emich.edu>
2025-02-19 13:21:10 -0500

    removed unused import

--- tests/login.test.tsx
+++ tests/login.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
