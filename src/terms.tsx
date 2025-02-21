12ec311ff4892487d77504f786a5cfafb1c8394a
joshrandall8478 <joshrandall8478@gmail.com>
2025-02-21 10:42:21 -0500

    added terms to site

---
+++ src/terms.tsx
@@ -0,0 +1,10 @@
+
+function Terms(){
+    return(
+        <div>
+            <h3 style={{ color: 'white' }}>No illegal content, otherwise go nuts</h3>
+        </div>
+    )
+}
+
+export default Terms;
