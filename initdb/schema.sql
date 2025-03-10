cb5e5b23c315366d63ced9f5f2b23f9ebf368162
Johnny Zheng <jzheng2@emich.edu>
2025-03-10 14:46:14 -0400

    added on delete cascade to foreign keys

--- initdb/schema.sql
+++ initdb/schema.sql
@@ -17,14 +17,14 @@ CREATE TABLE videos(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fileName text NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (creator_id) REFERENCES users(id)
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id int NOT NULL,
     content text NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE reply (
@@ -32,14 +32,14 @@ CREATE TABLE reply (
     creator_id int NOT NULL,
     content text NOT NULL,
     comment_id int NOT NULL,
-    FOREIGN KEY (comment_id) REFERENCES comments(id),
-    FOREIGN KEY (creator_id) REFERENCES users(id)
+    FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE reply_likes(
     id INT PRIMARY KEY AUTO_INCREMENT,
     reply_id INT NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY (reply_id) REFERENCES reply(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (reply_id) REFERENCES reply(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
