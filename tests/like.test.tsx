37ed38b4bad4cb5b7a5917b5a9b9e827c5243f38
Long Le <tlenguye@emich.edu>
2025-03-16 22:34:39 -0400

    import neccesery fw

---
+++ tests/like.test.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import axios from "axios";
+import LikeButton from "../src/likeButton";
