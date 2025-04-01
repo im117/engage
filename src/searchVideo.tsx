3268e36c939e3254cf1b6db3d75b66f64590f7c7
Long Le <tlenguye@emich.edu>
2025-04-01 16:58:01 -0400

    style

--- src/searchVideo.tsx
+++ src/searchVideo.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./styles/searchVideo.scss";
 
 // Define the upload server URL
 let uploadServer = "http://localhost:3001";
