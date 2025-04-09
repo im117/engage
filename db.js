7d2731a27ed4c7eccd2a8d5e2c2076f5f6186485
Joshua Randall <josh@joshrandall.net>
2025-04-09 17:44:50 -0400

    fixed port

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: 3306
   });
   
   // Connect to MySQL
